0867bacb81d4d89107bd6b37c5f530fe8db32f3a
Nathanage3 <sisay954@gmail.com>
2024-02-06 17:56:11 +0300

    new update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,14 +4,13 @@ import sys
 import os
 from datetime import datetime
 import inspect
-
 import pep8 as pycodestyle
 import time
 import unittest
 from unittest import mock
 
 
-sys.path.append(os.path.abspath('.'))
+#sys.path.append(os.path.abspath('.'))
 from models.base_model import BaseModel
 
 module_doc = BaseModel.__doc__

a6fa29b6a884543d97e95264c5742b42b1d45b58
Nathanage3 <sisay954@gmail.com>
2024-02-06 23:58:51 +0300

    api

--- test_get_count.py
+++ test_get_count.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Test .get() and .count() methods
 """
-from models import storage
+'''from models import storage
 from models.state import State
 
 print("All objects: {}".format(storage.count()))
@@ -9,3 +9,19 @@ print("State objects: {}".format(storage.count(State)))
 
 first_state_id = list(storage.all(State).values())[0].id
 print("First state: {}".format(storage.get(State, first_state_id)))
+'''
+#!/usr/bin/python3
+"""Doc
+"""
+from models.engine.db_storage import DBStorage
+
+
+class DBStorage(DBStorage):
+    """Doc
+    """
+
+    def get(self, cls, id):
+        """
+        returns the object by class and ID
+        """
+        return None
